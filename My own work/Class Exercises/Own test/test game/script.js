1dd7bb7882a40297ca4b9380ea717d338278457d
Tc <tanky0752@gmail.com>
2024-12-06 20:38:06 +0000

    12/6

--- My own work/Class Exercises/Own test/test game/script.js
+++ My own work/Class Exercises/Own test/test game/script.js
@@ -1,24 +1,56 @@
-let ball, floor;
+let player;
+let walls;
 
 function setup() {
-	new Canvas(238, 200);
-	world.gravity.y = 10;
+	new Canvas(1000, 1000);
+	world.gravity.y = 11;
+	player = new Sprite(100,100,25,25)
+	player.color = "red"
+	player.rotationLock=true;
+	player.drag = 1;
 
-	ball = new Sprite();
-	ball.diameter = 50;
-	ball.y = 30;
+	walls = new Group();
+	walls.w = 50
+	walls.h = 50 
+	walls.tile= "="
+	walls.collider = "static"
+	walls.drag = 10 ;
 
-	floor = new Sprite();
-	floor.y = 190;
-	floor.w = 238;
-	floor.h = 5;
-	floor.collider = 'static';
+	new Tiles(
+		["==========",
+		 "=........=",
+		"=........=",
+		"=...==...=",
+		"=.=......=",
+		"=========="],
+		50,
+		50,
+		walls.w,
+		walls.h,
+	)
 }
 
 function draw() {
 	clear();
+	background(0)
+	if (kb.pressing("a"))
+	{
+		player.x -= 3 ;
+		if (kb.presses("space")&&player.colliding(walls))
+		{
+			player.vel.y = -6 ;
+		}
+	}
+	else if (kb.pressing("d"))
+	{
+		player.x += 3 ;
+		if (kb.presses("space")&&player.colliding(walls))
+		{
+			player.vel.y = -6 ;
+		}
+	}
+	else if (kb.presses("space")&&player.colliding(walls))
+	{
+		player.vel.y = -6 ;
+	}
 }
-
-if (kb.pressing('left')) ball.vel.x = -5;
-else if (kb.pressing('right')) ball.vel.x = 5;
-else ball.vel.x = 0;
