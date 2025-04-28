d481a95b5156bd5423c5c595fad00e77fa58cc1e
TankyChan <hchan624@canon-slade.bolton.sch.uk>
2025-04-28 10:50:38 +0100

    04/28

--- Class Exercises/8 - Sorting Algorithms/Bubble Sort/Bubble Sort Assess/script.js
+++ Class Exercises/8 - Sorting Algorithms/Bubble Sort/Bubble Sort Assess/script.js
@@ -28,8 +28,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 
-    function bubbleSort(array) {
-  
+    function bubbleSort(array){
+        let n =array.length
+        let  swapped = false
+        do {
+            swapped = false
+            for (let i = 0; i<n-1; i++){
+                if (array[i]>array[i+1]){
+                    let temp = array[i]
+                    array[i]=array[i+1]
+                    array[i+1]=temp
+                    swapped = true
+                }
+            }
+            n--;
+        }while(swapped);
+        return array
     }
     
 
